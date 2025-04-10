46bcb028d4681cf9fc009ed983ccf4823b099444
KEERTHINAIK R <keerthinaik.r@outlook.com>
2025-04-10 23:05:40 +0530

    created a basic route

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 
 const dotenv = require('dotenv');
+
+// Setting up config.env files
 dotenv.config({path : './config/config.env'});
+
+// Importing routes
+const jobs = require('./routes/jobs');
+
+app.use('/api/v1',jobs);
+
+// app.use(jobs);
 
 const PORT = process.env.PORT;
 
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/jobs', (req, res) => {
     res.status(200).json({
         success: true,
         message: 'Jobs route is working'
     });
-}
-);
+});
+
+module.exports = router;
